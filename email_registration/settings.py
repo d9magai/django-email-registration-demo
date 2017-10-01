9335e1149a7f7e18f26aee95d2550c44ca18bf1d
d9magai <kumagai@sencorp.co.jp>
2017-10-02 01:46:23 +0900

    use mysql

--- email_registration/settings.py
+++ email_registration/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'email_registration.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'registration',
+        'USER': 'user',
+        'PASSWORD': 'secret',
+        'HOST': 'localhost',
+        'PORT': '3308',
     }
 }
 
